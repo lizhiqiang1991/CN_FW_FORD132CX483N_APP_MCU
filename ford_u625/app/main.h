417fd5114f04ed5493b0f0b5f78252bf2c857091
OrlandoHuang <Orlando.Huang@auo.com>
2022-07-20 10:02:35 +0800

    [modify] 1. Turns off fusa for I2C. 2. Updates .bin for memory address settings.
    Date:2022/07/20
    SW:0101
    Project:U717 & CX430

--- ford_u625/app/main.h
+++ ford_u625/app/main.h
@@ -28,7 +28,7 @@
 #define FORD_I2CCOMV1P9		true
 #define INT_TCH_LATCH       false
 #elif (CX430_TDDI_NT51926)
-#define FORD_SPSS_CRC_ROLL_EN  true
+#define FORD_SPSS_CRC_ROLL_EN  false
 #define FORD_SPSSV1P0		true
 #define FORD_SPSSV1P1		false
 #define FORD_I2CCOMV1P9		false
