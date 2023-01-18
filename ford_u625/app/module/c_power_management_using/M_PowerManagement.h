53c18606a9f90f97a7ee20583b3dffc29c608b73
daviddwliu <david.dw.liu@auo.com>
2023-01-18 09:29:21 +0800

    <Modify> Modify 12V Diagnostic Threshold.
    <Details>
    1.Modify 12V diagnostic threshold to 6.6v
    
    <TaskID> Null

--- ford_u625/app/module/c_power_management_using/M_PowerManagement.h
+++ ford_u625/app/module/c_power_management_using/M_PowerManagement.h
@@ -22,7 +22,7 @@
 #define	P3V3_PGOOD					0x00U
 #define	P1V2_PGOOD               	0x01U
 
-#define POWER_VMINRCV_ADC_CFG		775 /*Define as 7.0V*/
+#define POWER_VMINRCV_ADC_CFG		726 /*Define as 6.6V*/
 
 #define POWER_VMAXRCV_ADC_CFG		2114 /*Define as 18V*/
 
