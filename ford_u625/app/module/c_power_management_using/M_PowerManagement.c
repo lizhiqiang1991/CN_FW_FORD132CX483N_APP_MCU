a9dfba928a491dbcd801cc4be7acc45713a7fd56
michellejmwu <Michelle.JM.Wu@auo.com>
2023-04-10 14:34:53 +0800

    <Modify> Modify HV_LDO_EN delay 100ms for EE Test
    <Details>
    1. Modify HV_LDO_EN delay 100ms for HW Test
    2. Change version to T-03.03.03
    
    <TaskID> Null
    
    Change-Id: I4124250c07fdab009058423fb9a39088537f72f6

--- ford_u625/app/module/c_power_management_using/M_PowerManagement.c
+++ ford_u625/app/module/c_power_management_using/M_PowerManagement.c
@@ -149,7 +149,7 @@ uint8_t M_PM_Sequnce_Execute(uint8_t u8Action)
 			Cy_SysLib_Delay(6U); /* 6ms */
             HAL_GPIO_Low( U301_P3V3_EN_PORT, U301_P3V3_EN_PIN);
 
-			Cy_SysLib_Delay(55U); /* 55ms */
+			Cy_SysLib_Delay(100U); /* 100ms */
             HAL_GPIO_Low( U301_HV_LDO_EN_PORT, U301_HV_LDO_EN_PIN);
 
             HAL_GPIO_Low(EN_VBATT_SENSE_PORT,EN_VBATT_SENSE_PIN);
